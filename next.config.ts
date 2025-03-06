4535ca84b556b4f567c61f227a50606e6eaf6c11
subhodeep <sarkarsubhodeep417@gmail.com>
2025-03-06 21:02:02 +0530

    fixed nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,21 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true, // Disable ESLint during the build process
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Disable ESLint during the build process
+  },
+  images: {
+    domains: ["res.cloudinary.com"], // Allow Cloudinary images
   },
 };
 
